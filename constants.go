94d2aaaa90aee79622a904e9a2d018a8ad795e3c
Adam Chalkley <36716992+atc0005@users.noreply.github.com>
2020-01-16 12:45:49 -0600

    Convert sizeThreshold from a constant to a flag (#27)
    Set the default to 1 byte to mimic the prior constant's value.
    
    refs #24

--- constants.go
+++ constants.go
@@ -10,9 +10,3 @@ package main
 // DuplicatesThreshold is the number of files of the same file size needed
 // before duplicate validation logic is applied.
 const DuplicatesThreshold int = 2
-
-// SizeThreshold is the minimum size in bytes that a file must be before
-// it is added to our FileSizeIndex. This is an attempt to limit index entries
-// to just the files that are most relevant; the assumption is that zero-byte
-// files are not relevant.
-const SizeThreshold int64 = 1
