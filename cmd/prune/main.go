3083bd2f8c239bd4dcf6aeebab7f4cce2357f62b
Adam Chalkley <atc0005@users.noreply.github.com>
2020-02-16 05:55:46 -0600

    Stub out scratch notes

--- cmd/prune/main.go
+++ cmd/prune/main.go
@@ -24,4 +24,38 @@ func main() {
 
 	log.Printf("Configuration: %+v\n", appConfig)
 
+	// Values we can work with:
+	//
+	// config.IgnoreErrors
+	// config.DryRun
+	// config.BlankLineBetweenSets
+	// config.InputCSVFile
+	// config.ConsoleReport
+	// config.BackupDirectory
+	// config.PruneFiles
+
+	// -------------------------------------------------------------------- //
+
+	/*
+
+		STEPS
+
+		Parse config options
+		Open CSV file
+			? Buffered reader?
+		Create CSV Reader object
+		Apply CSV parsing requirements
+			- Require specific number of fields
+			- Skip blank lines
+		Loop over rows
+		Validate row fields
+			- Field content?
+				- e.g., checksum field has a length expected of current
+				  hash algorithm
+		Verify files exist
+		Verify checksum for each file removal candidate
+		Backup file removal candidate (if option is set)
+
+	*/
+
 }
