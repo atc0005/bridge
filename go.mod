31592f2070058ab83c59c19302118355d16ffbe8
Adam Chalkley <atc0005@users.noreply.github.com>
2020-02-18 06:29:15 -0600

    WIP: Copy/paste (with plans to modify) CleanPath function from elbow project

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.13
 require (
 	github.com/360EntSecGroup-Skylar/excelize v1.4.1
 	github.com/atc0005/elbow v0.7.1
+	github.com/sirupsen/logrus v1.4.2
 )
--- paths/paths.go
+++ paths/paths.go
@@ -13,6 +13,10 @@ import (
 	"log"
 	"os"
 	"strings"
+
+	"github.com/atc0005/bridge/config"
+	"github.com/atc0005/bridge/matches"
+	"github.com/sirupsen/logrus"
 )
 
 // PathExists confirms that the specified path exists
@@ -34,5 +38,77 @@ func PathExists(path string) bool {
 	}
 
 	return false
+
+}
+
+// CleanPath receives a slice of FileMatch objects and removes each file. Any
+// errors encountered while removing files may optionally be ignored via
+// command-line flag(default is to return immediately upon first error). The
+// total number of files successfully removed is returned along with an error
+// code (nil if no errors were encountered).
+func CleanPath(files matches.FileMatches, config *config.Config) (PathPruningResults, error) {
+
+	log := config.GetLogger()
+
+	for _, file := range files {
+		log.WithFields(logrus.Fields{
+			"fullpath":        strings.TrimSpace(file.Path),
+			"shortpath":       file.Name(),
+			"size":            file.Size(),
+			"modified":        file.ModTime().Format("2006-01-02 15:04:05"),
+			"removal_enabled": config.GetRemove(),
+		}).Debug("Matching file")
+	}
+
+	var removalResults PathPruningResults
+
+	if !config.GetRemove() {
+
+		log.Info("File removal not enabled, not removing files")
+
+		// Nothing to show for this yet, but since the initial state reflects
+		// that we can return it as-is
+		return removalResults, nil
+	}
+
+	for _, file := range files {
+
+		log.WithFields(logrus.Fields{
+			"removal_enabled": config.GetRemove(),
+
+			// fully-qualified path to the file
+			"file": file.Path,
+		}).Debug("Removing file")
+
+		// We need to reference the full path here, not the short name since
+		// the current working directory may not be the same directory
+		// where the file is located
+		err := os.Remove(file.Path)
+		if err != nil {
+			log.WithFields(logrus.Fields{
+
+				// Include full details for troubleshooting purposes
+				"file": file,
+			}).Errorf("Error encountered while removing file: %s", err)
+
+			// Record failed removal, proceed to the next file
+			removalResults.FailedRemovals = append(removalResults.FailedRemovals, file)
+
+			// Confirm that we should ignore errors (likely enabled)
+			if !config.GetIgnoreErrors() {
+				remainingFiles := len(files) - len(removalResults.FailedRemovals) - len(removalResults.SuccessfulRemovals)
+				log.Debugf("Abandoning removal of %d remaining files", remainingFiles)
+				break
+			}
+
+			log.Debug("Ignoring error as requested")
+			continue
+		}
+
+		// Record successful removal
+		removalResults.SuccessfulRemovals = append(removalResults.SuccessfulRemovals, file)
+	}
+
+	return removalResults, nil
 
 }
