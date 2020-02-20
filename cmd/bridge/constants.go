bdcd6b3ae09541550690c67269ee003927287342
Adam Chalkley <atc0005@users.noreply.github.com>
2020-02-20 12:45:00 -0600

    Add missing package declaration

--- cmd/bridge/constants.go
+++ cmd/bridge/constants.go
@@ -5,11 +5,13 @@
 // Licensed under the MIT License. See LICENSE file in the project root for
 // full license information.
 
+package main
+
 // Tabwriter header names displayed in console output
 const (
-	TabWriterDirectoryColumnHeaderName            string = "Directory"
-	TabWriterFileColumnHeaderName                 string = "File"
-	TabWriterSizeColumnHeaderName                 string = "Size"
-	TabWriterChecksumColumnHeaderName             string = "Checksum"
-	TabWriterRemoveFileColumnHeaderName           string = "Remove"
+	TabWriterDirectoryColumnHeaderName  string = "Directory"
+	TabWriterFileColumnHeaderName       string = "File"
+	TabWriterSizeColumnHeaderName       string = "Size"
+	TabWriterChecksumColumnHeaderName   string = "Checksum"
+	TabWriterRemoveFileColumnHeaderName string = "Remove"
 )
